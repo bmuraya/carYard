72ea9e0ad339272235bba23304c3100c99c0d2ae
bmuraya <babumuraya@gmail.com>
2025-01-06 21:35:38 +0300

    blade

---
+++ carYard/app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class HomeController extends Controller
+{
+     public function index()
+     {  
+        return  View::make('index')
+                            ->with('name', 'John Doe')
+                            ->with('age', 40)
+                            ->with('job', '<b> Software Developer </b>')
+                            ->with('hobbies', ['Reading', 'Swimming', 'Coding']);
+     }
+
+
+
+}
--- carYard/routes/web.php
+++ carYard/routes/web.php
@@ -1,29 +1,15 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 
 use Illuminate\Support\Facades\Route;
 use Psy\Command\WhereamiCommand;
 
-use App\Http\Controllers\MyController;
-use App\Http\Controllers\PostController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::view('/about', 'about')->name('about');
 
 
-Route::get('/', function () {
-//    $aboutPageUrl = "/about";
-
-
-    return view('welcome');
-});
-
-// Route::view('/about-us', 'about')->name('about');
-
-
-//  Route::get('/sum/{num1}/{num2}', function ( float $num1,  float $num2) {
-//      return $num1 + $num2;
-//  })->whereNumber(['num1', 'num2']);
-
-
-
-Route::get('/dashboard', [MyController::class, 'showDashboard']);
 
 
