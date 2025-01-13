eade9bb3da64eb1a737dd1fa025a44a9f170ab3d
bmuraya <babumuraya@gmail.com>
2025-01-13 21:24:02 +0300

    updates

--- carYard/app/Models/CarType.php
+++ carYard/app/Models/CarType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CarType extends Model
@@ -16,10 +17,8 @@ class CarType extends Model
         'name'
     ];
 
-    /**
-     * Define the relationship with the Car model.
-     * A CarType can have many Cars.
-     */
+
+
     public function cars(): HasMany
     {
         return $this->hasMany(Car::class);
