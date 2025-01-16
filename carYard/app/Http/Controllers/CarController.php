0cd2bdf5666c350f522db9f7130b91ef842f0958
bmuraya <babumuraya@gmail.com>
2025-01-16 22:15:34 +0300

    output content

--- carYard/app/Http/Controllers/CarController.php
+++ carYard/app/Http/Controllers/CarController.php
@@ -36,7 +36,8 @@ class CarController extends Controller
      */
     public function show(Car $car)
     {
-        return view('car.show');
+        
+        return view('car.show' ,[ 'car' => $car]);
     }
 
     /**
@@ -64,7 +65,19 @@ class CarController extends Controller
     }
 
     public function search()
-    {
-        return view('car.search');
+    {  
+       $query= Car::where('published_at', '<' , now())
+            ->orderBy('published_at', 'desc');
+
+            $carCount = $query->count();
+
+            $cars = $query->limit(30)->get();
+
+
+
+        return view('car.search', [
+            'cars' => $cars,
+            'carCount' => $carCount
+        ]);
     }   
 }
